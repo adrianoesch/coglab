2102178a160314f4e7eec6d4c65eed480f362a87
Adrian Oesch <adrianoesch@gmx.ch>
2015-10-22 17:09:50 +0200

    adjust store.php

--- store.php
+++ store.php
@@ -1,9 +1,14 @@
 <?php
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
-$filename = "data/".$_POST['filename'];
+$filename = $_POST['filename'];
+$folder = explode($filename)[0]
+$path = 'data/'.$folder
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+}
 $data = $_POST['filedata'];
 
 // write the file to disk
-file_put_contents($filename, $data);
+file_put_contents("data/".$filename, $data);
 ?>
