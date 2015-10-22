d38935d763cb9f8320e37255650dc4f0def3aef2
Adrian Oesch <adrianoesch@gmx.ch>
2015-10-22 18:28:46 +0200

    fix store.php

--- store.php
+++ store.php
@@ -3,5 +3,10 @@
 // the directory "data" is writable by the server (chmod 777)
 
 // write the file to disk
-file_put_contents($_POST['filename'], $_POST['filedata'],APPEND_FILE);
+$filename = $_POST['filename'];
+if (file_exists($filename)) {
+  file_put_contents($filename, $_POST['filedata'],APPEND_FILE);
+} else {
+  file_put_contents(, $_POST['filedata']);
+};
 ?>
