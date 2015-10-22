e0331fe284e406f75993778487d28120ea76633e
Adrian Oesch <adrianoesch@gmx.ch>
2015-10-22 17:13:16 +0200

    fix store.php

--- store.php
+++ store.php
@@ -2,7 +2,8 @@
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
 $filename = $_POST['filename'];
-$folder = explode($filename,'/')[0]
+$nameparts = explode($filename,'/')
+$foler = $nameparts[0]
 $path = 'data/'.$folder
 if (!file_exists($path)) {
     mkdir($path, 0777, true);
