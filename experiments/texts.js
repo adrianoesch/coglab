47bffbef869ccbb415a8712208141f7bd618165e
Adrian Oesch <adrianoesch@gmx.ch>
2015-11-17 23:33:18 +0100

    add instructions and texts

---
+++ experiments/texts.js
@@ -0,0 +1,58 @@
+texts = {};
+texts.warning = '<h2>Welcome</h2><p>To run this experiment we need launch into fullscreen mode. Following actions will terminate \
+                the experiment:<ul><li>exiting fullscreen mode</li><li>switching tabs or applications</li>\
+                <li>reloading the webpage</li><li>going-"back" in the browser</li></ul>\
+                <p>Please close all other running applications and turn off all notifications.</p><p>Ensure you\
+                are in a calm place without any distractions, for example, noise.<p>\
+                <p>Click "Enter" to launch into fullscreen and start experiment.'
+texts.informed_consent = '<h2>Consent</h2><p>In this experiment you will be asked to briefly remember short sets of words. \
+                          You will also make judgments on the words at the same time. The experiment will \
+                          take about 10 to 15 minutes. The data will be recorded anonymously. Your participation \
+                          is of course voluntary, and you can quit the experiment at any time without giving any \
+                          reasons. However, you will receive payment only if you complete the experiment.</p>\
+                          <p>By pressing the right arrow you agree to those terms and continue the experiment.'
+texts.instructions1 = '<h2>Instructions</h2><p>Thank you very much for taking part in our experiment! The experiment will take about \
+                      10 to 15 minutes.</p><p>In each trial you will see 10 words presented sequentially. All words \
+                      refer to concrete objects.</p></p>You have two tasks: <p>Your first task is to judge for each word \
+                      whether the object it refers to is larger or smaller than a soccer ball.</p><p> If the object is \
+                      <strong>smaller</strong> than a soccer ball, please press the <strong>left</strong> arrow key;</p>\
+                      <p>if the object is <strong>larger</strong> than a soccer ball, please press the \
+                      <strong>right</strong> arrow key.</p><p>Your second task is to remember all the \
+                      <strong>red words</strong> in their <strong>order of presentation</strong>. You don\'t need \
+                      to remember the blue words.<br>At the end of the presentation of all 10 words you will be \
+                      asked to select the red words from a set of 15 words. Please click on the 5 red words that \
+                      you remembered in their order of presentation.<br>If you do not remember all words nevertheless \
+                      try to select each word at the position it initially appeared. For example, if you do not remember \
+                      the third red word that was presented, but the fourth red word, please select the fourth word as the \
+                      fourth word.</p><p>You will first do two practice trials, followed by 12 serious trials.</p>\
+                      <p>Please press the right arrow key to continue ...</p>'
+
+texts.instruction_red = 'Please select the 5 red words, in their order of presentation, with the mouse'
+texts.instruction_blue = 'This time please recall the blue words, not the red words, in their order of presentation.'
+texts.instruction_circle = 'Continue by clicking on the blue circle.'
+
+texts.fullscreen_exit = 'You were not supposed to end fullscreen mode. <br>The session was terminated.'
+texts.visibility_exit = "You were not supposed to switch tabs or applications during the experiment.<br>The session was terminated."
+
+texts.visibility_fail = "Your Browser doesn't allow checking the browser's visibility. Please update your current browser or use another one for this experiment. We recommend using Chrome or Firefox."
+texts.fullscreen_fail = "Your Browser doesn't allow launching into fullscreen. Please update your current browser or use another one for this experiment. We recommend using Chrome or Firefox."
+
+texts.sincerity = '<p>Finally, we ask you to tell us honestly whether you did the experiment seriously, \
+                  giving it your full attention. You will receive your payment independent of how you respond, \
+                  this question serves only to decide whether your data will be included in the analysis.</p>\
+                  <p>Have you done the experiment seriously?</p>'
+texts.sincerity_yes = 'Yes, my data should be used'
+texts.sincerity_no = 'No, my data should not be used'
+
+texts.end_fullscreen = "This was it! Please save your data and continue to the debriefing/confirmation code\
+                       by clicking 'Save & Exit'."
+texts.debriefing = '<p>Thank you again for participating in our experiment.</p><p> The goal of this experiment was \
+                   to investigate whether people can intentionally forget information they want to forget, \
+                   and whether this is accomplished better when more time is available for forgetting. To \
+                   that end we tested, in the last trial, your memory for the blue words, which you were \
+                   supposed to forget. We varied the free time following each of the blue words - either only \
+                   200 ms or 1500 ms after each blue word. We expect that participants who had longer free \
+                   time following the blue words are able to remember fewer of them.</p><p>Please do not share \
+                   this information with anyone! It is crucial the explanation is kept confidential until we \
+                   have collected all data.</p><p>Here is your code to confirm your participation: WMCspan-2</p>\
+                   <p>You can now close the browser window/tab after copying the confirmation code.</p>'
