48e30bc10d05a115071c8bb6106c67db2471524d
Adrian Oesch <adrianoesch@gmx.ch>
2015-12-01 17:14:08 +0100

    store json with datestring

--- store2.php
+++ store2.php
@@ -8,9 +8,10 @@ $folder = $_POST['folder'];
 $csvData = $_POST['csvStrings'];
 $jsonData = $_POST['dataAsJSON'];
 $openshift_data_dir = $_ENV["OPENSHIFT_DATA_DIR"];
+$timestr = date('Y-m-d_H:i:s');
 
 //at first, write a backup copy of the json file into backup folder
-file_put_contents($openshift_data_dir.'json/'.uniqid().'.txt', $jsonData);
+file_put_contents($openshift_data_dir.'json/'.$timestr.'_'.uniqid().'.txt', $jsonData);
 
 // then put csvStrings in experiment folder.
 if(!file_exists($openshift_data_dir.'csv/'.$folder.'/')){ mkdir($openshift_data_dir.'csv/'.$folder.'/',0777,true);}
